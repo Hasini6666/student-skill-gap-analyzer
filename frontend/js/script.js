82bc1db327de0e9b27b6d5d68a869fb8c6f27065
Hasini Guttula <hasinig699@gmail.com>
2026-05-28 12:28:51 +0530

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,16 +3,106 @@ document
 .addEventListener("click", function () {
 
     let name =
-        document.getElementById("name").value;
+        document.getElementById("name")
+        .value;
 
     let skills =
-        document.getElementById("skills").value;
+        document.getElementById("skills")
+        .value
+        .toLowerCase()
+        .split(",");
 
     let role =
-        document.getElementById("role").value;
+        document.getElementById("role")
+        .value;
 
-    document.getElementById("result").innerHTML =
+    let roleSkills = {
+
+        "Java Developer": [
+            "java",
+            "sql",
+            "dsa",
+            "git",
+            "spring boot",
+            "rest apis"
+        ],
+
+        "Frontend Developer": [
+            "html",
+            "css",
+            "javascript",
+            "react",
+            "git"
+        ],
+
+        "Backend Developer": [
+            "java",
+            "sql",
+            "spring boot",
+            "rest apis",
+            "dbms"
+        ],
+
+        "Software Engineer": [
+            "java",
+            "dsa",
+            "dbms",
+            "git",
+            "sql"
+        ],
+
+        "Machine Learning Engineer": [
+            "python",
+            "machine learning",
+            "numpy",
+            "pandas"
+        ]
+    };
+
+    let requiredSkills =
+        roleSkills[role];
+
+    let matchedSkills = 0;
+    let missingSkills = [];
+
+    for (let i = 0;
+         i < requiredSkills.length;
+         i++) {
+
+        let skill =
+            requiredSkills[i];
+
+        if (skills.includes(skill)) {
+            matchedSkills++;
+        } else {
+            missingSkills.push(skill);
+        }
+    }
+
+    let score =
+        Math.round(
+            (matchedSkills /
+             requiredSkills.length)
+             * 100
+        );
+
+    document
+    .getElementById("result")
+    .innerHTML =
+
         "Hello " + name +
-        "! You selected " + role +
-        " and entered skills: " + skills;
+        "<br><br>" +
+
+        "Target Role: " +
+        role +
+
+        "<br><br>" +
+
+        "Readiness Score: "
+        + score + "%" +
+
+        "<br><br>" +
+
+        "Missing Skills: <br>"
+        + missingSkills.join(", ");
 });
